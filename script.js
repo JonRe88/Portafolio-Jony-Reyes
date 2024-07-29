c10fe4784c8a9488efaddc30256da79c96b3aa49
Jony Reyes <jreyesturismo@hotmail.com>
2024-07-28 21:22:18 -0600

    done
    done

---
+++ script.js
@@ -0,0 +1,30 @@
+// header scrolling effect
+$(window).on('scroll', function(){
+	if($(window).scrollTop()){
+      $('header').addClass('nav-show');
+		  
+	} 
+	else{
+		$('header').removeClass('nav-show');
+	}
+	   
+})
+
+		 
+      //Animation links
+	 navLinks.forEach((link, index) => {
+		if (link.style.animation) {
+			link.style.animation = "";
+		} else {
+			link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7+1}s`;
+		   }
+		});
+	// Example script file
+$(document).ready(function(){
+    $('.nav-link').click(function(){
+        $('.nav-link').removeClass('active');
+        $(this).addClass('active');
+    });
+});}
+
+window.onload = () => navSlide();
